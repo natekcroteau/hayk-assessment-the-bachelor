efcf70082ac62ed59551482a1887694b5014d62c
Nate Croteau <natekcroteau@gmail.com>
2020-12-10 16:52:49 -0700

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,64 @@
+require 'pry'
+
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.find do |season_number, contestants|
+    if season_number == season
+      contestants.find do |contestant|
+        contestant["status"] == "winner" 
+        return contestant["name"].split.first 
+      end 
+    end
+  end 
 end
+
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_number, contestants|
+    contestants.find do |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end 
+    end
+  end
 end
 
+
+
 def count_contestants_by_hometown(data, hometown)
-  # code here
+hometome_counter = 0
+  data.each do |season_number, contestants|
+    contestants.filter do |contestant|
+      if contestant["hometown"] == hometown
+        hometome_counter += 1
+      end
+    end
+  end
+  hometome_counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_number, contestants|
+    contestants.find do |contestant|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    end 
+  end 
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+ages = []
+  data.each do |season_number, contestants|
+    if season_number == season  
+      contestants.filter do |contestant|
+        ages << contestant["age"].to_f
+      end
+    end
+  end
+  avg=ages.sum/ages.length
+  avg.round
 end
+
+
+
